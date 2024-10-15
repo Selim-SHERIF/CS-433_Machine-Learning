b9c7a75c2ba13ba1164e44b0f125a2e555586bff
Selim SHERIF <selim.sherif@epfl.ch>
2024-10-15 15:07:45 +0200

    Lab 5 Done

--- labs/ex05/template/least_squares.py
+++ labs/ex05/template/least_squares.py
@@ -8,10 +8,22 @@ import numpy as np
 
 
 def least_squares(y, tx):
-    """calculate the least squares."""
+    """Calculate the least squares solution.
+       returns mse, and optimal weights.
+
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+
+    Returns:
+        w: optimal weights, numpy array of shape(D,), D is the number of features.
+        mse: scalar.
+
+    >>> least_squares(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]))
+    (array([ 0.21212121, -0.12121212]), 8.666684749742561e-33)
+    """
     # ***************************************************
-    # INSERT YOUR CODE HERE
-    # least squares: TODO
-    # returns mse, and optimal weights
+    w = np.linalg.solve(np.dot(tx.T, tx), np.dot(tx.T, y))
+    mse = np.sum((y - tx @ w) ** 2)/(tx.shape[0] *2 )
+    return w, mse
     # ***************************************************
-    raise NotImplementedError
